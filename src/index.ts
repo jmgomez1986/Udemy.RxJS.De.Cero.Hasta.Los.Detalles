76030a799f171cb977ee6d8964beb3c1b8632b26
jmgomez2018 <jmgametal@gmail.com>
2020-10-22 17:06:21 -0300

    Clase 67. Manejo de errores con el Fetch Api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 const url = 'https://api.github.com/usersx?per_page=5';
 
+const manejaErrores = (response: Response) => {
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response;
+}
+
 const fetchPromesa = fetch(url);
 
-fetchPromesa
-    .then(resp => resp.json())
-    .then(data => console.log('data:', data))
-    .catch(err => console.warn('error en usuarios', err))
+// fetchPromesa
+//     .then( resp => resp.json() )
+//     .then( data => console.log('data:', data) )
+//     .catch( err => console.warn('error en usuarios', err ) )
 
 fetchPromesa
+    .then(manejaErrores)
     .then(resp => resp.json())
     .then(data => console.log('data:', data))
     .catch(err => console.warn('error en usuarios', err))
