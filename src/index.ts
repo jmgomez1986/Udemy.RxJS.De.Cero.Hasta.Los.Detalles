541893a864cffe881e9cade900804bdd568c7832
jmgomez2018 <jmgametal@gmail.com>
2020-10-23 12:03:04 -0300

    Clase 77. Tipando los operadores

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,53 @@
-import { fromEvent } from 'rxjs';
-import { debounceTime, map, mergeAll, pluck } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, map, pluck, mergeAll } from 'rxjs/operators';
+
 import { ajax } from 'rxjs/ajax';
 
+import { GithubUser } from './interfaces/github-user.interface';
+import { GithubUsersResp } from './interfaces/github-users.interface';
+
+// Referencias
 const body = document.querySelector('body');
-const inputElement = document.createElement('input');
+const textInput = document.createElement('input');
 const orderList = document.createElement('ol');
+body.append(textInput, orderList);
 
-body.append(inputElement, orderList);
+// Helpers
+const mostrarUsuarios = (usuarios: GithubUser[]) => {
 
-const input$ = fromEvent<KeyboardEvent>(inputElement, 'keyup');
+    console.log(usuarios);
+    orderList.innerHTML = '';
+
+    for (const usuario of usuarios) {
+
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = usuario.avatar_url;
+
+        const anchor = document.createElement('a');
+        anchor.href = usuario.html_url;
+        anchor.text = 'Ver página';
+        anchor.target = '_blank';
+
+        li.append(img);
+        li.append(' ' + usuario.login + ' ');
+        li.append(anchor);
+
+        orderList.append(li);
+    }
+
+}
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup');
 
 input$.pipe(
-    debounceTime(500),
-    pluck('target', 'value'),
-    map(texto => ajax.getJSON(
+    debounceTime<KeyboardEvent>(500),
+    pluck<KeyboardEvent, string>('target', 'value'),
+    map<string, Observable<GithubUsersResp>>(texto => ajax.getJSON(
         `https://api.github.com/search/users?q=${texto}`
     )),
-    mergeAll(),
-    pluck('items')
-).subscribe(resp => {
-    console.log(resp);
-});
+    mergeAll<GithubUsersResp>(),
+    pluck<GithubUsersResp, GithubUser[]>('items')
+).subscribe(mostrarUsuarios);
+
