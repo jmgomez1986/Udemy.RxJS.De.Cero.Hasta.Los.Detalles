219adc00dc3ae615e61d0e36c38e8b4d7f1b17c4
jmgomez2018 <jmgametal@gmail.com>
2020-10-26 15:25:54 -0300

    Clase 88. startWith

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,8 @@
-import { fromEvent, of } from 'rxjs';
-import { tap, map, mergeMap, pluck, catchError, switchMap, exhaustMap } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
+import { of } from 'rxjs';
+import { startWith, endWith } from 'rxjs/operators';
 
-// Helper
-const peticionHttpLogin = (userPass) =>
-  ajax.post('https://reqres.in/api/login?delay=1', userPass)
-    .pipe(
-      pluck('response', 'token'),
-      catchError(err => of('xxx'))
-    )
+const numeros$ = of(1,2,3).pipe(
+    startWith(0)
+);
 
-// creando un formulario
-const form = document.createElement('form');
-const inputEmail = document.createElement('input');
-const inputPass = document.createElement('input');
-const submitBtn = document.createElement('button');
-
-// Configuraciones
-inputEmail.type = 'email';
-inputEmail.placeholder = 'Email';
-inputEmail.value = 'eve.holt@reqres.in';
-
-inputPass.type = 'password';
-inputPass.placeholder = 'Password';
-inputPass.value = 'cityslicka';
-
-submitBtn.innerHTML = 'Ingresar';
-
-form.append(inputEmail, inputPass, submitBtn);
-document.querySelector('body').append(form);
-
-// Streams
-const submitForm$ = fromEvent<Event>(form, 'submit')
-  .pipe(
-    tap(ev => ev.preventDefault()),
-    map(ev => ({
-      email: ev.target[0].value,
-      password: ev.target[1].value
-    })),
-    // mergeMap(peticionHttpLogin) // Hace una peticion por cada submit
-    // switchMap(peticionHttpLogin) // Cancela las peticiones activas y se queda con la ultima
-    exhaustMap(peticionHttpLogin) // Solo hace la primer peticion
-  );
-
-submitForm$.subscribe(token => {
-  console.log(token);
-})
+numeros$.subscribe( console.log );
